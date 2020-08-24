6882d068d0a855e29392e335739df3d717e18508
Asis98 <asiacasella@gmail.com>
2020-08-24 15:14:46 +0200

    modifiche ore 15.14 24-08

--- Versione_5/src/gestioneMenu/M_Visualizza_Categorie.java
+++ Versione_5/src/gestioneMenu/M_Visualizza_Categorie.java
@@ -1,5 +1,11 @@
 package gestioneMenu;
 
-public class M_Visualizza_Categorie {
+public class M_Visualizza_Categorie implements MenuCommand{
+
+	@Override
+	public void esegui() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
